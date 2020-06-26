4439c67fedec3f0f256fee2dd23456f8a93f5930
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-06-26 18:26:34 -0500

    implemented error handling middleware for mongoose validation, general errors and not found errors

--- middleware.js
+++ middleware.js
@@ -1,5 +1,10 @@
+const { STATUS_CODES } = require('http')
+
 module.exports = {
-  cors
+  cors,
+  handleError,
+  notFound,
+  handleValidationError
 }
   
 function cors (req, res, next) {
@@ -18,4 +23,23 @@ function cors (req, res, next) {
   )
 
   next()
+}
+
+function handleError (err, req, res, next) {
+  console.error(err)
+  if (res.headersSent) return next(err)
+
+  const statusCode = err.statusCode || 500
+  const errorMessage = STATUS_CODES[statusCode] || 'Internal Error'
+  res.status(statusCode).json({ error: errorMessage })
+}
+
+function notFound (req, res) {
+  res.status(404).json({ error: 'Not Found' })
+}
+
+function handleValidationError (err, req, res, next) {
+  if (err.name !== 'ValidationError') return next(err)
+
+  res.status(400).json({ error: err._message, errorDetails: err.errors })
 }
