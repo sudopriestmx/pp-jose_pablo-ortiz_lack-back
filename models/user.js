732fad08db93ef3d235b5cbcc774cde170e473d6
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-06-26 17:07:04 -0500

    implemented User model with CRUD

---
+++ models/user.js
@@ -0,0 +1,85 @@
+const cuid = require('cuid');
+const { isEmail, isMobilePhone, contains } = require('validator');
+const moment = require('moment');
+
+const db = require('../db');
+
+const User = db.model('User', {
+    _id: { type: String, default: cuid },
+    name: { type: String, required: True, index: true},
+    email: emailSchema({required: true}),
+    phone: phoneSchema({required: true}),
+    password: { type: String, required: True},
+    age: { type: Number, required: true},
+    gender: {type: String, enum: ['Masculino', 'Femenino', 'Otro'] },
+    hobby: {type: String, required: true, index: true},
+    registrationDate: {type: Date, required: true, default: Date.now(), index: true}
+
+});
+
+module.exports = {
+    list,
+    create,
+    remove,
+    model: User
+  }
+
+async function list (opts = {}) {
+    const { name, hobby } = opts;
+    let query;
+    if (name) {
+        if (hobby) {
+            query = {
+                name: name,
+                hobby: hobby
+            };
+        } else {
+            query = {
+                name: name
+            };
+        }
+    } else {
+        query = {};
+    }
+
+    const users = await User.find(query)
+        .sort({registrationDate: 1});
+    
+    return users;
+}
+
+async function create (fields) {
+    const user = await new User(fields).save();
+
+    return user;
+}
+
+async function remove (_id) {
+
+    await User.deleteOne({ _id })
+}
+
+function emailSchema (opts = {}) {
+    const { required } = opts
+    return {
+        type: String,
+        required: !!required,
+        validate: {
+            validator: isEmail,
+            message: props => `${props.value} no es un correo válido`
+        }
+    }
+}
+
+function phoneSchema (opts = {}) {
+    const { required } = opts
+    return {
+        type: String,
+        required: !!required,
+        validate: {
+            validator: isMobilePhone,
+            message: props => `${props.value} no es un teléfono válido`
+        }
+    }
+
+}
