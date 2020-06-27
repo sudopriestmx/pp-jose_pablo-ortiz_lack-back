77e3b454b0df4e60100733990a761fc173279e04
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-06-26 22:21:54 -0500

    removed debugging console log

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -4,7 +4,6 @@ const router = require('express').Router();
 //proyect modules
 const auth = require('../auth');
 
-console.log('authRouter');
 router.post('/', auth.authenticate, auth.login);
 
 module.exports = router;
