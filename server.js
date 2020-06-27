5afb9c44d228a97c646926e5ea046287986786b0
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-06-26 20:22:22 -0500

    added users router and middleware to server

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const bodyParser = require('body-parser');
 
 //proyect modules
 const middleware = require('./middleware');
+const usersRouter = require('./routers/usersRouter');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(middleware.cors);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/users', usersRouter);
+app.use(middleware.handleValidationError);
+app.use(middleware.handleError);
+app.use(middleware.notFound);
+
 
 const server = app.listen(port, () =>
   console.log(`Server listening on port ${port}`)
