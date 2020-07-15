780f62fc389f78d630de08f0488a9592d0f884cc
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-07-14 20:44:28 -0500

    make it better

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGO_URI || 'mongodb://localhost:27017/pandp',
+  process.env.MONGO_URI || 'mongodb+srv://joselack:super-secret-password@cluster0.g6psj.azure.mongodb.net/test',
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
