02af8de8aa16176fbdbdd38bfab79afd437bed90
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-08-12 14:31:04 -0500

    typo fix

--- routers/mercadoPagoRouter.js
+++ routers/mercadoPagoRouter.js
@@ -8,7 +8,7 @@ router.post ('/', async (req, res, next) => {
     try {
 
       res.status(200).send('');
-      
+
       let url = ''
       switch(req.query.topic) {
         case 'payment':
@@ -18,7 +18,7 @@ router.post ('/', async (req, res, next) => {
           url = `https://api.mercadopago.com/v1/chargebacks/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
           break
         case 'merchant_order':
-          url = `https://api.mercadopago.com/v1/mercant_orders/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
+          url = `https://api.mercadopago.com/v1/merchant_orders/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
           break
       }
       const response = await axios.get(url)
