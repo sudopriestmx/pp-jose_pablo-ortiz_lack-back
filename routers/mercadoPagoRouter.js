03a87bc693fadcb3043f852a9606770a52b0ab1a
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-08-12 15:58:12 -0500

    bug fix

--- routers/mercadoPagoRouter.js
+++ routers/mercadoPagoRouter.js
@@ -7,16 +7,18 @@ function MercadoPago() {
 
   const MercadoPago = require('mercadopago')
 
-  MercadoPago.configurations.setAccessToken('TEST-7625462056535872-081219-c8d0619f977ab3527c9f43253bbe8b55-625046243')
-
+  MercadoPago.configure({
+    sandbox: true,
+    access_token: "TEST-7625462056535872-081219-c8d0619f977ab3527c9f43253bbe8b55-625046243"
+  })
   return MercadoPago
 }
 
 router.post ('/', async (req, res, next) => {
 
     try {
-
       res.status(200).send('');
+      console.log(req.query)
       const ipn = await MercadoPago().ipn.manage(req)
       console.log(ipn)
 
