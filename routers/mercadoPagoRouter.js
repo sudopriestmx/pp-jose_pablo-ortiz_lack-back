83e1f166ae70d2c12b97467a33982ccf55b0bc27
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-08-12 14:28:49 -0500

    bug fixes

--- routers/mercadoPagoRouter.js
+++ routers/mercadoPagoRouter.js
@@ -6,25 +6,23 @@ const MERCADO_PAGO_ACCESS_TOKEN = process.env.MERCADO_PAGO_ACCESS_TOKEN
 router.post ('/', async (req, res, next) => {
 
     try {
-        console.log('**********QUERY**********', req.query)
-        console.log('********** PARAMS **********', req.params)
-        if((!req.query.topic && !req.query.id) || (!req.params.topic && !req.params.id))
-          throw new Error("Notificacion incorrecta")
-        res.status(200).send('');
-        let url = ''
-        switch(req.query.topic) {
-          case 'payment':
-            url = `https://api.mercadopago.com/v1/payments/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
-            break
-          case 'chargebacks':
-            url = `https://api.mercadopago.com/v1/chargebacks/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
-            break
-          case 'merchant_order':
-            url = `https://api.mercadopago.com/v1/mercant_orders/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
-            break
-        }
-        const response = await axios.get(url)
-        console.log(response)
+
+      res.status(200).send('');
+      
+      let url = ''
+      switch(req.query.topic) {
+        case 'payment':
+          url = `https://api.mercadopago.com/v1/payments/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
+          break
+        case 'chargebacks':
+          url = `https://api.mercadopago.com/v1/chargebacks/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
+          break
+        case 'merchant_order':
+          url = `https://api.mercadopago.com/v1/mercant_orders/${req.query.id}?access_token=${MERCADO_PAGO_ACCESS_TOKEN}`
+          break
+      }
+      const response = await axios.get(url)
+      console.log(response)
 
     } catch(err) {
         console.error(err)
