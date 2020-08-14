8727cc82177a6e58f7b71897b314872c5a451d44
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2020-08-14 16:45:25 -0500

    payment detail print test

--- routers/mercadoPagoRouter.js
+++ routers/mercadoPagoRouter.js
@@ -21,8 +21,30 @@ router.post ('/', async (req, res, next) => {
       console.log(req.query)
       console.log(req.body)
       const payment = await MercadoPago().payment.get(req.body.data.id)
-      console.log(payment)
-      console.log(payment.body.fee_details)
+      console.log('Description: ', payment.description)
+      console.log('user_id: ', payment.body.external_referece)
+      console.log('amount: ', payment.body.transaction_amount)
+      let mercadopago_fee = 0;
+      let pack_and_pack_amount = 0
+      for(let feeDetail in payment.body.fee_details) {
+        switch(feeDetail.type) {
+          case 'mercadopago_fee':
+            mercadopago_fee = feeDetail.amount
+            break
+          case 'application_fee':
+            pack_and_pack_amount = feeDetail.amount
+            break
+          case 'financing_fee':
+            console.log('pago a meses por el usuario cuesta: ', feeDetail.amount)
+            break
+          default:
+            console.log('FeeDetail Desconocido', feeDetail.amount)
+        }
+      }
+      console.log('pack_and_pack_amount: ', pack_and_pack_amount)
+      console.log('dagpacket_amount: ', payment.body.transaction_amount - mercadopago_fee - pack_and_pack_amount)
+      console.log('reference: ', payment.body.id)
+      console.log('created', new Date(payment.body.date_created))
     } catch(err) {
         console.error(err)
     }
